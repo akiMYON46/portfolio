f8b1a9a2864b5269728cb57fef38a00fe9e5d4aa
akiMYON46 <akiMYONmilk@gmail.com>
2024-07-29 08:11:03 +0900

    完成形
    残るは細かい調整のみ

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector('#menu-icon').addEventListener('click', (ev) => {
+    const navbar = document.querySelector('#navbar');
+    const menuicon = document.querySelector('#menu-icon');
+  
+    navbar.classList.toggle('active');
+  
+    if (navbar.classList.contains('active')) {
+      menuicon.textContent = '×';
+    } else {
+      menuicon.textContent = '☰';
+    }
+  });
